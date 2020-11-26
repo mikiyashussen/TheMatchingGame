fe654686f9fe8326f29691c62656f3eb447741e4
mikiyashussen <mikiyashussen19@gmail.com>
2020-11-26 17:30:25 +0100

    new game button

--- game.js
+++ game.js
@@ -1,19 +1,65 @@
-// access the card 
-let firstCard = document.querySelector(".card-1-front");
-let backCard = document.querySelector(".card-1-back")
 
-firstCard.addEventListener('click', function() {
-    firstCard.classList.add("hidden");
-    backCard.classList.remove("hidden");
+//.............Eventlistener for every Card.........
+let cards = document.querySelectorAll(".single-card");
+let classArray = [];
+function flipCard(card) {
 
+    console.log(card);
+    classArray.push(card.className);
+    card.children[1].classList.toggle("hidden");
+    card.children[0].classList.toggle("hidden");
+
+    // check for array length and if 2 ,do comparision
+    if(classArray.length === 2){
+        if(classArray[0] === classArray[1]){
+            console.log("cards Equal");
+            // count score
+        }
+        else{
+            console.log("cards not Equal");
+            // flip back cards
+            
+        }
+        classArray.length = 0;
+    }
+}
+cards.forEach(function (item) {
+    let clickCount = 0;
+    item.addEventListener("click", function() {
+        // debugger;
+        // console.log(item);
+        flipCard(item);
+        
+        
+    })
 });
 
-backCard.addEventListener('click', function() {
-    backCard.classList.add("hidden");
-    firstCard.classList.remove("hidden");
-
-});
 
 
 
-// flip when clicked
+
+
+// ...................NEW GAME BUTTON....................//
+let newGameBtn = document.querySelector('.new-game');
+
+newGameBtn.addEventListener('click', newGame);
+
+function newGame() {
+let cards = document.querySelectorAll(".single-card");
+    
+    // debugger;
+    cards.forEach(function(item) {
+        item.children[1].classList.add("hidden");
+        item.children[0].classList.remove("hidden");
+        
+    })
+    
+}
+
+// const cards = document.querySelectorAll(".single-card");
+
+// function flipCard() {
+//     debugger;
+//   this.classList.toggle("hidden");
+// }
+// cards.forEach((card) => card.addEventListener("click", flipCard))
